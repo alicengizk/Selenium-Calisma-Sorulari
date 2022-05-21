942a14471205e4bf74cca0ca85c714733805d4ea
alicengizk <alicengizk@gmail.com>
2022-05-21 20:37:07 +0300

    soru 16 bitti

--- src/test/java/Selenium_Calisma_Sorulari/Soru15.java
+++ src/test/java/Selenium_Calisma_Sorulari/Soru15.java
@@ -1,6 +1,11 @@
 package Selenium_Calisma_Sorulari;
 
-public class Soru15 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class Soru15 extends TestBase {
     /*
     1. http://zero.webappsecurity.com sayfasina gidin
     2. Signin buttonuna tiklayin
@@ -13,4 +18,29 @@ public class Soru15 {
     9. Pay buttonuna tiklayin
     10. “The payment was successfully submitted.” mesajinin ciktigini control edin
      */
+
+    @Test
+    public void test01() {
+        driver.get("http://zero.webappsecurity.com");
+        driver.findElement(By.xpath("//button[@id='signin_button']")).click();
+        driver.findElement(By.xpath("//input[@id='user_login']")).sendKeys("username");
+        driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("password");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        driver.navigate().back();
+        driver.findElement(By.xpath("//li[@id='onlineBankingMenu']")).click();
+        driver.findElement(By.xpath("//span[@id='pay_bills_link']")).click();
+        driver.findElement(By.xpath("//input[@id='sp_amount']")).sendKeys("50");
+        driver.findElement(By.xpath("//input[@id='sp_date']")).sendKeys("2020-09-10");
+        driver.findElement(By.xpath("//input[@class='btn btn-primary']")).click();
+        String expectedPaymentSubmitted="The payment was successfully submitted.";
+        String actualPaymentSubmitted=driver.findElement(By.xpath("//*[text()='The payment was successfully submitted.']")).getText();
+        Assert.assertEquals(expectedPaymentSubmitted,actualPaymentSubmitted);
+
+
+
+
+
+
+
+    }
 }
--- src/test/java/Selenium_Calisma_Sorulari/Soru16.java
+++ src/test/java/Selenium_Calisma_Sorulari/Soru16.java
@@ -1,6 +1,14 @@
 package Selenium_Calisma_Sorulari;
 
-public class Soru16 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class Soru16 extends TestBase {
     /*
     1-C01_TekrarTesti isimli bir class olusturun
     2- https://www.google.com/ adresine gidin
@@ -11,4 +19,23 @@ public class Soru16 {
     7- sonuc sayisinin 10 milyon’dan fazla oldugunu test edin
     8-Sayfayi kapatin
      */
+
+    @Test
+    public void test01() {
+        driver.get("https://www.google.com/");
+        String expectedTitle="Google";
+        String actualTitle=driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("Nutella"+ Keys.ENTER);
+        String sonucSayisi=driver.findElement(By.xpath("//div[@id='result-stats']")).getText();
+        System.out.println("Nutella Arama Sonuc : "+sonucSayisi);
+        String sonucsayilari=sonucSayisi.replaceAll("\\D", "");
+        double cikanRakam=Double.parseDouble(sonucsayilari);
+        if (cikanRakam/10000000>1){
+            System.out.println("Cikan Sonuc 10 Milyondan Fazla");
+        }else {
+            System.out.println("Cikan Sonuc 10 Milyondan Az");
+        }
+
+    }
 }
